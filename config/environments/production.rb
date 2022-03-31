8610b33ca83bc8d629c68b30e401a004dd876738
mdm33 <61870098+mdm33@users.noreply.github.com>
2022-03-31 13:15:10 +0100

    Update to login/security

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Proiel::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -49,7 +49,7 @@ Proiel::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
